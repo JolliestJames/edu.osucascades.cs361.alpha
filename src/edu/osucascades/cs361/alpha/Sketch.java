b4b4df4a9b9e2ff3ad13dda800c0607a3bf5d3b7
ToBeAmped <martinja@oregonstate.edu>
2018-03-03 12:33:21 -0800

    Add drawCircle method for each group member, implement drawJamesCircle method

--- src/edu/osucascades/cs361/alpha/Sketch.java
+++ src/edu/osucascades/cs361/alpha/Sketch.java
@@ -18,5 +18,22 @@ public class Sketch extends PApplet
     {
         fill(255, 255, 0);
         ellipse(width/2, height/2, 100, 100);
+        drawJamesCircle();
+    }
+
+    private void drawJamesCircle()
+    {
+        fill(220, 220, 125);
+        ellipse(width/4, height/4, 150, 150);
+    }
+
+    private void drawLeoCircle()
+    {
+
+    }
+
+    private void drawNathanCircle()
+    {
+        
     }
 }
