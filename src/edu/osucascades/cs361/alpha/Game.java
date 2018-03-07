a32d4b04275a2e431130e7ccea27c1e38a1ace85
Leo G <leo.gothberg@gmail.com>
2018-03-07 15:23:04 -0800

    Adstract game into Game class

--- src/edu/osucascades/cs361/alpha/Game.java
+++ src/edu/osucascades/cs361/alpha/Game.java
@@ -1,20 +1,57 @@
 package edu.osucascades.cs361.alpha;
 
+import java.util.*;
 import processing.core.PApplet;
 
 public class Game {
 
     PApplet p;
+    Ship ship;
+    ArrayList<Fort> forts = new ArrayList();
+    ArrayList<AlienRed> redAliens = new ArrayList();
+    ArrayList<AlienYellow> yellowAliens = new ArrayList();
+    ArrayList<AlienGreen> greenAliens = new ArrayList();
+    ArrayList<AlienBlue> blueAliens = new ArrayList();
 
-    public Game() {
-        // constructor
+    public Game (PApplet pApplet){
+        // initializes a new game
+        p = pApplet;
+        ship = new Ship(p);
+        forts.add(new Fort(p, p.width/10, p.height-p.height/4));
+        forts.add(new Fort(p, p.width/3, p.height-p.height/4));
+        forts.add(new Fort(p, p.width - p.width/3 - p.width/8, p.height-p.height/4));
+        forts.add(new Fort(p, p.width - p.width/10 - p.width/8, p.height-p.height/4));
+        for (int i = 0; i < 10; i++) {
+            redAliens.add(new AlienRed (p, 100, 100));
+            yellowAliens.add(new AlienYellow (p, 100, 100));
+            greenAliens.add(new AlienGreen (p, 100, 100));
+            blueAliens.add(new AlienBlue (p, 100, 100));
+        }
+
+//        p.fullScreen();
+        p.background(55);
     }
 
     public void update() {
         // update all objects in the game
+        if (p.keyPressed) {
+            if (p.key == 'd') {
+                ship.moveRight();
+            }
+            if (p.key == 'a') {
+                ship.moveLeft();
+            }
+        }
     }
 
     public void draw() {
-        // draw all game objects
+        // draw all objects in the game
+        p.background(55);
+        ship.draw();
+        for( Fort fort : forts) {
+            fort.draw();
+        }
+
+
     }
 }
--- src/edu/osucascades/cs361/alpha/Sketch.java
+++ src/edu/osucascades/cs361/alpha/Sketch.java
@@ -1,53 +1,21 @@
 package edu.osucascades.cs361.alpha;
 import processing.core.PApplet;
-import java.util.*;
 
-public class Sketch extends PApplet
-{
-    Ship ship;
-    ArrayList<Fort> forts = new ArrayList();
-    ArrayList<AlienRed> redAliens = new ArrayList();
-    ArrayList<AlienYellow> yellowAliens = new ArrayList();
-    ArrayList<AlienGreen> greenAliens = new ArrayList();
-    ArrayList<AlienBlue> blueAliens = new ArrayList();
+public class Sketch extends PApplet {
 
-    public void settings()
-    {
+    Game game;
+
+    public void settings() {
         fullScreen();
     }
 
-    public void setup()
-    {
-        background(55);
-        ship = new Ship(this);
-        forts.add(new Fort(this, width/10, height-height/4));
-        forts.add(new Fort(this, width/3, height-height/4));
-        forts.add(new Fort(this, width - width/3 - width/8, height-height/4));
-        forts.add(new Fort(this, width - width/10 - width/8, height-height/4));
-        for (int i = 0; i < 10; i++) {
-            redAliens.add(new AlienRed (this, 100, 100));
-            yellowAliens.add(new AlienYellow (this, 100, 100));
-            greenAliens.add(new AlienGreen (this, 100, 100));
-            blueAliens.add(new AlienBlue (this, 100, 100));
-        }
+    public void setup() {
+        game = new Game(this);
+        game.draw();
     }
 
-    public void draw()
-    {
-        background(55);
-        ship.draw();
-        for( Fort fort : forts) {
-            fort.draw();
-        }
-        for ()
-    }
-
-    public void keyPressed() {
-        if (key == 'd') {
-            ship.moveRight();
-        }
-        if (key == 'a') {
-            ship.moveLeft();
-        }
+    public void draw() {
+        game.update();
+        game.draw();
     }
 }
