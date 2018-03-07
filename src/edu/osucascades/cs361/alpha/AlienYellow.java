c686988751ab6af8ca2dbf1b6c858885de6be5c1
Nathan <hildebrn@oregonstate.edu>
2018-03-07 15:13:35 -0800

    Add Alien classes

---
+++ src/edu/osucascades/cs361/alpha/AlienYellow.java
@@ -0,0 +1,42 @@
+package edu.osucascades.cs361.alpha;
+
+import processing.core.PApplet;
+
+public class AlienYellow {
+
+    PApplet p;
+    String type;
+    int x, y, damage, speed, pointValue;
+
+    public AlienYellow(PApplet pApplet, int xPos, int yPos) {
+        p = pApplet;
+        x = xPos;
+        y = yPos;
+        damage = 0;
+
+        // need some logic to determine point value here
+        // pretty sure this will be linked to the Alien type
+        // will ask Yong about this on Wednesday; believe a switch statement is best
+    }
+
+    public void moveLeft() {
+        // logic to move left goes here
+    }
+
+    public void moveRight() {
+        // logic to move right goes here
+    }
+
+    public void moveDown() {
+        // logic to move down goes here
+        // this should increase speed every time it triggers
+    }
+
+    public void dropBomb() {
+        // logic to drop bomb goes here
+    }
+
+    public void draw() {
+        // logic to draw Alien goes here
+    }
+}
--- src/edu/osucascades/cs361/alpha/Sketch.java
+++ src/edu/osucascades/cs361/alpha/Sketch.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Sketch extends PApplet
 {
     Ship ship;
-    ArrayList<Fort> forts = new ArrayList<>();
+    ArrayList<Fort> forts = new ArrayList();
     ArrayList<AlienRed> redAliens = new ArrayList();
     ArrayList<AlienYellow> yellowAliens = new ArrayList();
     ArrayList<AlienGreen> greenAliens = new ArrayList();
@@ -24,6 +24,12 @@ public class Sketch extends PApplet
         forts.add(new Fort(this, width/3, height-height/4));
         forts.add(new Fort(this, width - width/3 - width/8, height-height/4));
         forts.add(new Fort(this, width - width/10 - width/8, height-height/4));
+        for (int i = 0; i < 10; i++) {
+            redAliens.add(new AlienRed (this, 100, 100));
+            yellowAliens.add(new AlienYellow (this, 100, 100));
+            greenAliens.add(new AlienGreen (this, 100, 100));
+            blueAliens.add(new AlienBlue (this, 100, 100));
+        }
     }
 
     public void draw()
@@ -33,6 +39,7 @@ public class Sketch extends PApplet
         for( Fort fort : forts) {
             fort.draw();
         }
+        for ()
     }
 
     public void keyPressed() {
